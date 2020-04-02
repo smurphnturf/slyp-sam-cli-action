53c834f3433c7a2ba5b38f5f940f582a51cb5716
Sean Murphy <sean@Seans-MacBook-Pro.local>
2020-04-03 07:15:58 +1100

    add build as an input and defaults to true

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,54 +3,59 @@
 set -e
 
 function gotoDirectory {
-        if [ -z "${INPUT_DIRECTORY}" ]; then
-                return 0
-        fi
+         if [ -z "${INPUT_DIRECTORY}" ]; then
+                  return 0
+         fi
 
-        if [ ! -d "${INPUT_DIRECTORY}" ]; then
-                echo "Directory ${INPUT_DIRECTORY} does not exists."
-                exit 127
-        fi
+         if [ ! -d "${INPUT_DIRECTORY}" ]; then
+                  echo "Directory ${INPUT_DIRECTORY} does not exists."
+                  exit 127
+         fi
 
-        echo "cd ${INPUT_DIRECTORY}"
-        cd $INPUT_DIRECTORY
+         echo "cd ${INPUT_DIRECTORY}"
+         cd $INPUT_DIRECTORY
+}
+
+function buildDirectory {
+         if [ "${INPUT_BUILD}" = "true" ]; then
+
+            # Ensures a lambda layer is built properly
+            if [ -d "./code/dependencies/nodejs" ] 
+            then
+               echo "running npm install on lambda layer in ./code/dependencies/nodejs"
+               cd ./code/dependencies/nodejs
+               npm install
+               cd ../../..
+            else
+               echo "No layer to build in ./code/dependencies/nodejs"
+            fi
+
+            # Ensures a stand alone layer is built properly
+            if [ -d "./dependencies/nodejs" ] 
+            then
+               if [ -d "./code" ]
+               then
+                  echo "running npm install on stand alone layer in ./code"
+                  cd ./code
+                  npm install
+                  cd ..
+               else
+                  echo "No layer to build in ./code"
+               fi
+               echo "running npm install on layer ./dependencies/nodejs"
+               cd ./dependencies/nodejs
+               npm install
+               cd ../..
+            else
+               echo "No layer to build in ./dependencies/nodejs"
+            fi
+            echo "executing sam build"
+            exec sam build
+         fi
 }
 
 gotoDirectory
-
-if [ $@ = "build" ]; then
-
-   # Ensures a lambda layer is built properly
-   if [ -d "./code/dependencies/nodejs" ] 
-   then
-      echo "running npm install on lambda layer in ./code/dependencies/nodejs"
-      cd ./code/dependencies/nodejs
-      npm install
-      cd ../../..
-   else
-      echo "No layer to build in ./code/dependencies/nodejs"
-   fi
-
-   # Ensures a stand alone layer is built properly
-   if [ -d "./dependencies/nodejs" ] 
-   then
-      if [ -d "./code" ]
-      then
-         echo "running npm install on stand alone layer in ./code"
-         cd ./code
-         npm install
-         cd ..
-      else
-         echo "No layer to build in ./code"
-      fi
-      echo "running npm install on layer ./dependencies/nodejs"
-      cd ./dependencies/nodejs
-      npm install
-      cd ../..
-   else
-      echo "No layer to build in ./dependencies/nodejs"
-   fi
-fi
+buildDirectory
 
 echo "executing sam $@"
 exec sam "$@"
